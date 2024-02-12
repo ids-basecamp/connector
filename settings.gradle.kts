590417e67fffb20ab6d71c3115fb7af16e614a9a
Glaucio Jannotti <glaucio.jannotti@dengun.com>
2024-02-11 22:45:29 -0300

    feat: included edc-extensions dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,19 +13,10 @@
  *
  */
 
-
 dependencyResolutionManagement {
     repositories {
         val user = providers.gradleProperty("gitHubUserName")
         val token = providers.gradleProperty("gitHubUserPassword")
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/sovity/edc-extensions")
-            credentials {
-                username = user.orNull
-                password = token.orNull
-            }
-        }
         maven {
             url = uri("https://maven.pkg.github.com/ids-basecamp/gradle-plugins-fork")
             credentials {
@@ -61,33 +52,46 @@ dependencyResolutionManagement {
         val edcVersion = providers.gradleProperty("edcVersion")
         create("edc") {
             version("edc", edcVersion.get())
-            library("spi-catalog", edcGroup.get(), "catalog-spi").versionRef("edc")
-            library("spi-core", edcGroup.get(), "core-spi").versionRef("edc")
-            library("spi-web", edcGroup.get(), "web-spi").versionRef("edc")
-            library("util", edcGroup.get(), "util").versionRef("edc")
+
             library("boot", edcGroup.get(), "boot").versionRef("edc")
-            library("config-filesystem", edcGroup.get(), "configuration-filesystem").versionRef("edc")
-            library("core-controlplane", edcGroup.get(), "control-plane-core").versionRef("edc")
             library("core-connector", edcGroup.get(), "connector-core").versionRef("edc")
-            library("core-jetty", edcGroup.get(), "jetty-core").versionRef("edc")
             library("core-jersey", edcGroup.get(), "jersey-core").versionRef("edc")
-            library("junit", edcGroup.get(), "junit").versionRef("edc")
-            library("api-management-config", edcGroup.get(), "management-api-configuration").versionRef("edc")
-            library("api-management", edcGroup.get(), "management-api").versionRef("edc")
+            library("core-controlplane", edcGroup.get(), "control-plane-core").versionRef("edc")
             library("api-observability", edcGroup.get(), "api-observability").versionRef("edc")
-            library("ext-http", edcGroup.get(), "http").versionRef("edc")
-            library("spi-ids", edcGroup.get(), "ids-spi").versionRef("edc")
-            library("ids", edcGroup.get(), "ids").versionRef("edc")
-            library("ids-jsonld-serdes", edcGroup.get(), "ids-jsonld-serdes").versionRef("edc")
+
+            library("api-management", edcGroup.get(), "management-api").versionRef("edc")
+            library("config-filesystem", edcGroup.get(), "configuration-filesystem").versionRef("edc")
             library("oauth2-core", edcGroup.get(), "oauth2-core").versionRef("edc")
+            library("iam-mock", edcGroup.get(), "iam-mock").versionRef("edc")
             library("vault-filesystem", edcGroup.get(), "vault-filesystem").versionRef("edc")
+
+            // Data Plane
+            library("dpf-core", edcGroup.get(), "data-plane-core").versionRef("edc")
+            library("dpf-selector-core", edcGroup.get(), "data-plane-selector-core").versionRef("edc")
+            library("dpf-selector-client", edcGroup.get(), "data-plane-selector-client").versionRef("edc")
+            library("dpf-framework", edcGroup.get(), "data-plane-framework").versionRef("edc")
+            library("dpf-http", edcGroup.get(), "data-plane-http").versionRef("edc")
+
+            // SQL Stores
+            library("sql-dataplane", edcGroup.get(), "data-plane-instance-store-sql").versionRef("edc")
+            library("sql-asset", edcGroup.get(), "asset-index-sql").versionRef("edc")
+            library("sql-policy", edcGroup.get(), "policy-definition-store-sql").versionRef("edc")
+            library("sql-contract-negotiation", edcGroup.get(), "contract-negotiation-store-sql").versionRef("edc")
+            library("sql-contract-definition", edcGroup.get(), "contract-definition-store-sql").versionRef("edc")
+            library("sql-transfer-process", edcGroup.get(), "transfer-process-store-sql").versionRef("edc")
 
             bundle(
                 "connector",
                 listOf("boot", "core-connector", "core-jersey", "core-controlplane", "api-observability")
             )
+
+            bundle(
+                "dpf",
+                listOf("dpf-core", "dpf-selector-core", "dpf-selector-client", "dpf-framework")
+            )
         }
     }
 }
 
-include(":connector-prod")
+include(":launchers:connector-local")
+include(":launchers:connector-prod")
